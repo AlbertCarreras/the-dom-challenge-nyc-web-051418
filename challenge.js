d8409a293e618d3dda4b40fe4cfd5573e31f42fc
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-29 15:01:43 -0400

    Solved 1.1

--- challenge.js
+++ challenge.js
@@ -1,60 +1,54 @@
-var ctr = 0
-var counter = document.getElementById('counter');
-var plus = document.getElementById('+');
-var minus = document.getElementById('-');
-var like = document.getElementById('<3');
-var likeList = document.getElementsByClassName('likes')[0];
-var pause = document.getElementById('pause');
-var commentForm = document.getElementById('comment-form');
-var submit = document.getElementById('submit');
-var commentList = document.getElementsByClassName('comments')[0];
-var go = true;
+//objects from html
+let counter = document.getElementById('counter');
+let plus = document.getElementById('+');
+let minus = document.getElementById('-');
+let like = document.getElementById('<3');
+let likeList = document.getElementsByClassName('likes')[0];
+let pause = document.getElementById('pause');
+document.getElementById("comment-form")[0].id = 'comment-input'; //add id to input field on form
+let commentForm = document.getElementById('comment-input'); //object with just added id
+let submit = document.getElementById('submit');
+let commentList = document.getElementsByClassName('comments')[0];
 
-  function timer() { //starts timer immediately
-    plus.addEventListener('click', function() { //increments up on + click
-      ctr++
-    })
-    minus.addEventListener('click', function() { //increments down on - click
-      ctr--
-    })
-      setInterval(function(){
-        if (go) { //pauses the counter when pause is clicked
-          counter.innerHTML = ctr++; //increments each second
-        console.log(go) }
+let ctr = 0 // object as counter
+let go = true; // object to store timer activation status
+function timer() { //starts timer immediately
+  plus.addEventListener('click', () => ctr++ ); //increments up on + click
+  minus.addEventListener('click', () => ctr-- );//increments down on - click
+  setInterval( () => {
+    if (go) { //pauses the counter when pause btn is clicked ang go turns false
+      counter.innerHTML = ctr++; //increments each second
+    }}, 1000);
+};
+timer();
 
-      }, 1000)
-  };
-  timer();
+let likesObj = {} //object for storing likes
+like.addEventListener('click', () => {
+  likesObj[ctr] ? likesObj[ctr]++ : likesObj[ctr] = 1; //starts at one or increments if exists
+  if (!document.getElementById(`${ctr}`)) {
+    likeList.innerHTML += `<li id=${ctr}>${ctr} has been liked times ${likesObj[ctr]}</li>`;
+  } else {
+    document.getElementById(ctr).innerHTML = `${ctr} has been liked times ${likesObj[ctr]}`;
+  }
+})
 
-  likesObj = {} //object for storing likes
-    like.addEventListener('click', function() {
-      likesObj[ctr] ? likesObj[ctr]++ : likesObj[ctr]= 1 //starts at one or increments if exists
-      if (!document.getElementById(`${ctr}`)) {
-      likeList.innerHTML += `<li id=${ctr}>${ctr} has been liked times ${likesObj[ctr]}</li>`
-    } else {
-      document.getElementById(ctr).innerHTML = `${ctr} has been liked times ${likesObj[ctr]}`
-    }
-  })
+pause.addEventListener('click', () => { //pauses buttons
+  if (pause.innerHTML === "pause") {
+    go = false;
+    pause.innerHTML = "resume";
+    plus.disabled = true;
+    minus.disabled = true;
+    like.disabled = true;
+  } else {
+    go = true;
+    pause.innerHTML = "pause";
+    plus.disabled = false;
+    minus.disabled = false;
+    like.disabled = false;
+  }
+})
 
-  pause.addEventListener('click', function() { //pauses buttons
-    if (pause.innerHTML === "pause") {
-      pause.innerHTML = "resume";
-      go = false;
-      plus.disabled = true;
-      minus.disabled = true;
-      like.disabled = true;
-    } else {
-      pause.innerHTML = "pause";
-      go = true;
-      plus.disabled = false;
-      minus.disabled = false;
-      like.disabled = false;
-    }
-
-  })
-
-  submit.addEventListener('click', function(e) {
-    e.preventDefault();
-    // console.log(commentForm.value)
-    commentList.innerHTML += `<li>${commentForm.children[0].value}</li>`
-  });
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  commentList.innerHTML += `<li>${commentForm.value}</li>`;
+});
